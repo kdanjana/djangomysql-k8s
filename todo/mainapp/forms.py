7e5423198e804a8f096d6ef0bb76da1a520fa23f
anjana <kad.an.devi@gmail.com>
2023-09-05 13:00:27 +0530

    deleted extra form

--- todo/mainapp/forms.py
+++ todo/mainapp/forms.py
@@ -1,32 +1,6 @@
-rom django import forms
-
+from django import forms
 from .models import Item
 
-class ExForm(forms.Form):
-    myfield = forms.CharField(widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
-
-class TodoForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ["todo"]
-        labels = {
-            "todo": "Todo"
-        }
-        widgets = {
-            "todo": forms.TextInput(attrs={'class': 'myfieldclass'})
-        }
-
-
-    def clean(self):
-        super(TodoForm, self).clean()
-        todo = self.cleaned_data.get('todo')
-        if len(todo) < 3:
-            self._errors['todo'] = self.error_class([rom django import forms
-
-from .models import Item
-
-class ExForm(forms.Form):
-    myfield = forms.CharField(widget=forms.TextInput(attrs={'class': 'myfieldclass'}))
 
 class TodoForm(forms.ModelForm):
     class Meta:
